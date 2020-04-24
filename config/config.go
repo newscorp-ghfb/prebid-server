f1a69d1c955e06a83aedadb725cbe17385c980c7
Kamal Saini <harkamals@live.com>
2020-04-24 12:56:45 -0400

    Update config.go

--- config/config.go
+++ config/config.go
@@ -578,9 +578,9 @@ func SetupViper(v *viper.Viper, filename string) {
 	v.SetDefault("status_response", "")
 	v.SetDefault("auction_timeouts_ms.default", 0)
 	v.SetDefault("auction_timeouts_ms.max", 0)
-	v.SetDefault("cache.scheme", "")
-	// var pbCacheHost = os.Getenv("PBS_CACHE_HOST")
-	var pbCacheHost = os.Getenv("PBS_CACHE_ENDPOINT")
+	v.SetDefault("cache.scheme", "https")
+	var pbCacheHost = os.Getenv("PBS_CACHE_HOST")
+	// var pbCacheHost = os.Getenv("PBS_CACHE_ENDPOINT")
 
 	v.SetDefault("cache.host", pbCacheHost)
 	v.SetDefault("cache.query", "")
